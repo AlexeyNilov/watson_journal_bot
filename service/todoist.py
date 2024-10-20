dacb24e30ecca78b65b467b64319229e99954f2f
alexey.nilov <alexey.nilov@ringcentral.com>
2024-10-20 17:19:28 +0200

    change priority logic

--- service/todoist.py
+++ service/todoist.py
@@ -9,15 +9,14 @@ from conf.settings import TDIST_API_TOKEN
 CLIENT = TodoistAPIAsync(TDIST_API_TOKEN)
 
 
-def get_priority(content: str) -> int | None:
-    for priority in range(1, 5):
-        if f"p{priority}" in content:
-            return priority
-    return None
-
-
 async def add_task(content: str):
-    priority = get_priority(content)
-    content = content.replace(f"p{priority}", "")
+    content = content.strip()
+    priority = 1
+    if ".urg" in content:
+        priority += 1
+        content = content.replace(".urg", "")
+    if ".imp" in content:
+        priority += 1
+        content = content.replace(".imp", "")
     content = content.strip()
     await CLIENT.add_task(content=content, labels=["from_watson"], priority=priority)
