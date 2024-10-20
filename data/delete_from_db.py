682b82a9bbfaf506d09027c12c51269f0422de5d
alexey.nilov <alexey.nilov@ringcentral.com>
2024-10-20 15:55:24 +0200

    fix like

--- data/delete_from_db.py
+++ data/delete_from_db.py
@@ -1,7 +1,7 @@
 from data.fastlite_db import DB
 
 t = DB.t.event
-query = "SELECT * FROM event WHERE text LIKE 'I feel';"
+query = "SELECT * FROM event WHERE text LIKE '%I feel%';"
 events = DB.q(query)
 for event in events:
     t.delete(event["id"])
